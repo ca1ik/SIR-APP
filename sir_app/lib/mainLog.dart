1f7d8bcdacdd91e860f7da75b8e93f105c7ab9a7
Halil Calik <halicalix@gmail.com>
2025-09-12 02:41:46 +0300

    Create mainLog.dart

---
+++ sir_app/lib/mainLog.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+var logger = Logger();
+
+void main() {
+  runApp(const MyApp());
+
+  // Örnek loglar
+  logger.d("Debug log");
+  logger.i("Info log");
+  logger.w("Warning log");
+  logger.e("Error log");
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Log Demo',
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Log Kontrol")),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              logger.i("Butona basıldı!");
+            },
+            child: const Text("Log Gönder"),
+          ),
+        ),
+      ),
+    );
+  }
+}
