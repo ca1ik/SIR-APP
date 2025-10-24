3f9f43d1ddb0c2a9dab2484175e2cc0a84992e6c
Halil Calik <halicalix@gmail.com>
2025-10-25 00:28:46 +0300

    Update halisaha.py

--- halisaha.py
+++ halisaha.py
@@ -6,6 +6,7 @@ class FutbolSahasiUygulamasi:
     def __init__(self, root):
         self.root = root
         self.root.title("Halı Saha Diziliş Uygulaması")
+        self.root.geometry("800x600")
 
         # Arka plan resmi yükleme
         self.saha_image_path = "halisaha.jpg"
